ac3ee93794b4cdbc9d742b426d4d20b2cd8591a7
Celso Matias <celsomatias7@gmail.com>
2024-04-16 12:23:16 -0300

    commit inicial

--- gruntfile.js
+++ gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
                     compress: true,
                 },
                 files: {
-                    'dist/styles/main.min.css' : 'src/styles/main.less'
+                    'dev/styles/main.min.css' : 'src/styles/main.less'
                 }
             }
         },
@@ -26,68 +26,10 @@ module.exports = function(grunt) {
                 tasks: ['replace:dev']
             }
         },
-        replace: { //criando um html de dev e production
-            dev: {
-                options: {
-                    patterns: [
-                        {
-                            match: 'ENDERECO_DO_CSS',
-                            replacement: './styles/main.css'
-                        },
-                        {
-                            match: 'ENDERECO_DO_JS',
-                            replacement: '../src/scripts/main.js'
-                        }
-                    ]
-                },
-                files: [
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['src/index.html'],
-                        dest: 'dev/'
-                    }
-                ]
-            },
-            dist: { //mandando o html minificado para a produção
-                options: {
-                    patterns: [
-                        {
-                            match: 'ENDERECO_DO_CSS',
-                            replacement: './styles/main.min.css'
-                        },
-                        {
-                            match: 'ENDERECO_DO_JS',
-                            replacement: './scripts/main.min.js'
-                        }
-                    ]
-                },
-                files: [
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['prebuild/index.html'],
-                        dest: 'dist/'
-                    }
-                ]
-            }
-        },
-        htmlmin: { //minificando o html
-            dist: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: {
-                    'prebuild/index.html': 'src/index.html' //primeiro irá minificar e depois mandar o arquivo alterado e minificado para o destino
-                }
-            }
-        },
-        clean: ['prebuild'], //para excluir a pasta temporária depois que a execução for realizada
         uglify: {
             target: {
                 files: {
-                    'dist/scripts/main.min.js' : 'src/scripts/main.js' //sempre primeiro coloca o destino e depois o arquivo origem, lembrando que este plugin é para minificar o arquivo
+                    'dev/scripts/main.min.js' : 'src/scripts/main.js' //sempre primeiro coloca o destino e depois o arquivo origem, lembrando que este plugin é para minificar o arquivo
                 }
             }
         }
@@ -101,5 +43,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     
     grunt.registerTask('default' , ['watch']);
-    grunt.registerTask('build' , ['less:production', 'htmlmin:dist', 'replace:dist' , 'clean', 'uglify']);
+    grunt.registerTask('build' , ['less:production', 'uglify']);
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('form-sorteador').addEventListener('submit' , function(evento) {
+    document.getElementById('num-qualquer').addEventListener('submit' , function(evento) {
         evento.preventDefault();
         let numeroMaximo = document.getElementById('numero-maximo').value;
         numeroMaximo = parseInt(numeroMaximo);
 
-        let numeroAleatorio = Math.random() * numeroMaximo;
-        numeroAleatorio = Math.round(numeroAleatorio);
-        
+        let numeroAleatorio = Math.floor(numeroMaximo);
+                
         document.getElementById('resultado-valor').innerText = numeroAleatorio
         document.querySelector('.resultado').style.display = 'block'; //para mostrar o resultado apenas após clicar em sortear numero
     })
